55c480b93f4f87be55ea465b7d9c8dd23b2ce9c6
William Galvis <williamsoluntech@gmail.com>
2024-10-09 17:58:53 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: '01_camera',
+  webDir: 'dist'
+};
+
+export default config;
