bbef9fc083808f3d80bdb6fe9b5e3c1bcc7615f1
William Galvis <williamsoluntech@gmail.com>
2024-10-09 19:09:17 -0500

    initial commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,15 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: '01_camera',
-  webDir: 'dist'
+    appId: "camera.es.app",
+    appName: "Magic Camera",
+    webDir: "dist",
+    plugins: {
+        LocalNotifications: {
+            smallIcon: "ic_stat_icon_config_sample",
+            iconColor: "#488AFF"
+        },
+    },
 };
 
 export default config;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,92 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import {
+    IonButton,
+    IonContent,
+    IonHeader,
+    IonIcon,
+    IonPage,
+    IonTitle,
+    IonToolbar,
+} from "@ionic/react";
+import ExploreContainer from "../components/ExploreContainer";
+import { Camera, CameraResultType, CameraSource } from "@capacitor/camera";
+import "./Home.css";
+import { useEffect, useState } from "react";
+import { camera } from "ionicons/icons";
+import { LocalNotifications } from "@capacitor/local-notifications";
 
 const Home: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
-      </IonContent>
-    </IonPage>
-  );
+    const [photo, setPhoto] = useState<string | undefined>(undefined);
+    useEffect(() => {
+        // Solicitar permiso para notificaciones al cargar el componente
+        LocalNotifications.requestPermissions();
+    }, []);
+
+    const takePicture = async () => {
+        try {
+            const image = await Camera.getPhoto({
+                quality: 90,
+                allowEditing: false,
+                resultType: CameraResultType.DataUrl,
+                source: CameraSource.Camera,
+            });
+
+            setPhoto(image.dataUrl);
+
+			sendNotification()
+        } catch (error) {
+            console.error("Error al tomar la foto:", error);
+        }
+    };
+
+    const sendNotification = async () => {
+        try {
+            await LocalNotifications.schedule({
+                notifications: [
+                    {
+                        title: "Foto Capturada desde Camara magica",
+                        body: "Has tomado una nueva foto con éxito",
+                        id: 1,
+                        schedule: { at: new Date(Date.now() + 1000) },
+                        actionTypeId: "",
+                    },
+                ],
+            });
+        } catch (error) {
+            console.error("Error al enviar la notificación:", error);
+        }
+    };
+
+    return (
+        <IonPage>
+            <IonContent className="ion-padding" fullscreen>
+                <div className="photo-container">
+                    {photo ? (
+                        <img
+                            src={photo}
+                            width={200}
+                            alt="Foto capturada"
+                            className="photo"
+                        />
+                    ) : (
+                        <div className="default-photo">
+                            <img
+                                src="/assets/default-photo.webp"
+                                alt="Foto por defecto"
+                                width={200}
+                                className="photo"
+                            />
+                            <p>Aquí aparecerá tu foto</p>
+                        </div>
+                    )}
+                </div>
+                <div className="button-container">
+                    <button onClick={takePicture} className="custom-button">
+                        <IonIcon icon={camera} />
+                    </button>
+                </div>
+            </IonContent>
+        </IonPage>
+    );
 };
 
 export default Home;
